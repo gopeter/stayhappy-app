4aa2f371015c10691521186ac4165959a4650dac
Peter Oesteritz <peter.oesteritz@debd.com>
2025-10-09 19:27:56 +0200

    fixed: widget image filename

--- Widgets/Views/HighlightsTile.swift
+++ Widgets/Views/HighlightsTile.swift
@@ -17,25 +17,27 @@ struct HighlightsTile: View {
     var highlight: Moment
     var photoImage: UIImage?
     var size: ImageSize
-    
+
     init(highlights: [Moment], size: ImageSize) {
         self.highlight = highlights[0]
         self.size = size
 
         if highlight.photo != nil {
-            let photoUrl = FileManager.documentsDirectory.appendingPathComponent("\(String(describing: highlight.photo!))-\(size == .systemMedium ? "_widget_2x1" : "_widget_1x1").jpg")
+            let photoUrl = FileManager.documentsDirectory.appendingPathComponent(
+                "\(String(describing: highlight.photo!))\(size == .systemMedium ? "_widget_2x1" : "_widget_1x1").jpg"
+            )
             self.photoImage = UIImage(contentsOfFile: photoUrl.path)
         }
     }
 
     var body: some View {
         GeometryReader { geometry in
-            
+
             RoundedRectangle(cornerRadius: 0, style: .continuous)
                 .fill(
                     photoImage == nil
-                    ? HappyGradients(rawValue: highlight.background)!.radial(startRadius: -50, endRadius: geometry.size.width)
-                    : RadialGradient(gradient: Gradient(colors: [.clear, .clear]), center: .center, startRadius: 0, endRadius: 0)
+                        ? HappyGradients(rawValue: highlight.background)!.radial(startRadius: -50, endRadius: geometry.size.width)
+                        : RadialGradient(gradient: Gradient(colors: [.clear, .clear]), center: .center, startRadius: 0, endRadius: 0)
                 )
                 .frame(maxWidth: .infinity)
                 .background {
@@ -57,14 +59,14 @@ struct HighlightsTile: View {
                                     .font(.caption)
                                     .shadow(color: .black.opacity(0.4), radius: 2, x: 0, y: 1)
                                     .padding(0)
-                                
+
                                 Text(highlight.title)
                                     .fontWeight(.bold)
                                     .foregroundStyle(.white)
                                     .shadow(color: .black.opacity(0.4), radius: 2, x: 0, y: 1)
                                     .padding(0)
                             }
-                            
+
                             Spacer()
                         }
                     }
